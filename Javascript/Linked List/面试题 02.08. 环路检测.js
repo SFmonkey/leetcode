96267ed5a8a21a6bdd59c3a99c7cb6606c9c4b43
LH <15091762986@163.com>
2020-05-26 10:59:49 +0800

    环路监测使用快慢指针方法

--- Javascript/Linked List/面试题 02.08. 环路检测.js
+++ Javascript/Linked List/面试题 02.08. 环路检测.js
@@ -11,20 +11,31 @@
  * @return {ListNode}
  */
 /**
- * 给每个节点添加一个标志位pos
- * 如果某个节点已经有pos标志位了  说明有环  返回该节点
- * 如果循环自己跳出，则返回null
-*/
+ * 快慢指针方法
+ * 以数组值为指针将数组类比为链表
+ * fast 每次走两步，slow每次走一步
+ * 当第一次fast == slow时，将快指针置为0
+ * 快指针和慢指针每次移动一位当再次相遇时即为重复值
+ * 
+ * 注：第一次相遇时，设环长为m， 非环部分为n, 相遇在k处， 则快指针走了 n+B*m+k   慢指针走了 n+A*m+k
+ * 快指针 - 慢指针 = (B-A) * m
+ * 所以快慢指针都走了环长的倍数，所以当fast到起始点时走非环部分n时，慢指针也就刚好走到环的重复位置
+ * 即此时快慢指针指向的即为环的起始值
+ */
 var detectCycle = function(head) {
-    let p = head
-    let i = 0
-    while(p) {
-        if(p.pos == undefined) {
-            p.pos = i++
-            p = p.next
-        }else{
-            return p
+    let slow = head;
+    let fast = head;
+    while(fast && fast.next && fast.next.next) {
+        fast = fast.next.next;
+        slow = slow.next;
+        if(slow == fast) {
+            fast = head
+            while(slow !== fast){
+                fast = fast.next;
+                slow = slow.next;
+            }
+            return fast
         }
-    }   
+    }  
     return null
 };
