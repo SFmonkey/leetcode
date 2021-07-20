41cc5d03486b3a1468fa3a6a3dddbe810678bcc8
houxinglin <houxinglin@houxinglindeMacBook-Pro.local>
2021-07-20 15:54:53 +0800

    two add

--- Go/ArrayList/2-两数相加.go
+++ Go/ArrayList/2-两数相加.go
@@ -9,30 +9,40 @@ type ListNode struct {
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	//var ans *ListNode	// todo: * 和 & 在声明变量时的区别
-	ans := ListNode{
-		Val:  0,
-		Next: nil,
-	}
-	var head ListNode
-	head = ans
-	f := 0
-	for l1 != nil && l2 != nil {
-		tmp := &ListNode{}
-		v := l1.Val + l2.Val + f
-		if v >= 10 {
+	var add, f int
+	head := &ListNode{}
+	ans := head
+	for l1 != nil || l2 != nil {
+		add = 0
+		add += f
+		if l1 != nil {
+			add += l1.Val
+			l1 = l1.Next
+		}
+		if l2 != nil {
+			add += l2.Val
+			l2 = l2.Next
+		}
+		if add >= 10 {
 			f = 1
-			v = v - 10
+			add -= 10
 		} else {
 			f = 0
 		}
-		tmp.Val = v
-		ans.Next = tmp
-		ans = *ans.Next
-		l1 = l1.Next
-		l2 = l2.Next
+		tmp := ListNode{
+			Val:  add,
+			Next: nil,
+		}
+		head.Next = &tmp
+		head = head.Next
 	}
-	head = *head.Next
-	return &head
+	if 1 == f {
+		head.Next = &ListNode{
+			Val:  1,
+			Next: nil,
+		}
+	}
+	return ans.Next
 }
 
 func pprint(l *ListNode) {
--- Go/ArrayList/2-两数相加_test.go
+++ Go/ArrayList/2-两数相加_test.go
@@ -5,29 +5,25 @@ import "testing"
 func TestTwoAdd(t *testing.T)  {
 	// 2 -> 4 -> 3
 	// 5 -> 6 -> 4
-	c := ListNode{
-		Val:  3,
+	b := ListNode{
+		Val:  9,
 		Next: nil,
 	}
-	b := ListNode{
-		Val:  4,
-		Next: &c,
-	}
 	a := ListNode{
-		Val: 2,
+		Val: 9,
 		Next: &b,
 	}
 
 	f := ListNode{
-		Val:  4,
+		Val:  9,
 		Next: nil,
 	}
 	e := ListNode{
-		Val:  6,
+		Val:  9,
 		Next: &f,
 	}
 	d := ListNode{
-		Val: 5,
+		Val: 9,
 		Next: &e,
 	}
 	res := addTwoNumbers(&a, &d)
