b5b5456b10c9eab74bccef4f074fc00d318bc5e6
dylanhouxinglin <houxinglin@gmai.com>
2021-08-18 20:18:55 +0800

    47-全排列II-未注释

--- Go/ArrayList/47-全排列II.go
+++ Go/ArrayList/47-全排列II.go
@@ -1,20 +1,18 @@
 package ArrayList
 
 import (
-	"fmt"
 	"sort"
 )
 
 var res47 [][]int
 var path47 []int
 var tmp47 []int
-var repeat map[string]bool
-var str string
+var visit []bool
 
 func permuteUnique(nums []int) [][]int {
 	res47 = [][]int{}
 	path47 = []int{}
-	repeat = make(map[string]bool)
+	visit = make([]bool, len(nums))
 	sort.Ints(nums)
 	backTrackingPU(nums, 0)
 	return res47
@@ -25,26 +23,20 @@ func backTrackingPU(nums []int, startIndex int)  {
 		tmp47 = make([]int, len(path47))
 		copy(tmp47, path47)
 		res47 = append(res47, tmp47)
-		str = ""
-		for j:=0; j<len(tmp47); j++ {
-			str += fmt.Sprintf("%d", tmp47[j])
-		}
-		repeat[str] = true
 		return
 	}
 	for i:=startIndex; i<len(nums); i++ {
-		path47 = append(path47, nums[i])
-		str = ""
-		for j:=0; j<len(path47); j++ {
-			str += fmt.Sprintf("%d", path47[j])
-		}
-		fmt.Println(str)
-		if _, ok := repeat[str]; ok {
-			path47 = path47[:len(path47)-1]
+		if visit[i] {
 			continue
 		}
+		if i > 0 && nums[i] == nums[i-1] && !visit[i-1] {
+			continue
+		}
+		path47 = append(path47, nums[i])
+		visit[i] = true
 		backTrackingPU(nums, 0)
 		path47 = path47[:len(path47)-1]
+		visit[i] = false
 	}
 }
 
