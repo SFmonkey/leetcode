948f4486763e7518a1926596954e8f3d0948758f
dylanhouxinglin <houxinglin@gmai.com>
2021-08-25 11:48:06 +0800

    func removeDuplicates2(nums []int) int {

---
+++ Go/ArrayList/80-删除数组中的重复项II.go
@@ -0,0 +1,17 @@
+package ArrayList
+
+// 快慢指针，后向前覆盖
+func removeDuplicates2(nums []int) int {
+	if len(nums) <= 2 {
+		return len(nums)
+	}
+	// j指向需要被替换的元素，i去寻找替换j的元素
+	j := 2
+	for i:=2; i<len(nums); i++ {
+		if nums[i] != nums[j-2] {
+			nums[j] = nums[i]
+			j++
+		}
+	}
+	return j
+}
