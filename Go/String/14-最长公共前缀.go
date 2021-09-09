4e52e897cf0f1ab3460059a9c77f0573c21d23f7
dylanhouxinglin <houxinglin@gmail.com>
2021-09-10 07:43:43 +0800

    update

--- Go/String/14-最长公共前缀.go
+++ Go/String/14-最长公共前缀.go
@@ -7,13 +7,15 @@ func longestCommonPrefix(strs []string) string {
 	}
 	i := 0
 	ans := ""
-	s := strs[0][0]
 	for {
-		for j:=0; j<len(strs); j++ {
+		if i >= len(strs[0]) {
+			return ans
+		}
+		s := strs[0][i]
+		for j:=1; j<len(strs); j++ {
 			if i >= len(strs[j]) || strs[j][i] != s {
 				return ans
 			}
-			s = strs[j][i]
 		}
 		ans += string(s)
 		i++
