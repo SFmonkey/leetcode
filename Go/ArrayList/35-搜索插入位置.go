d42b95d4fc266ea33306ae5b88dcd1545c20ef33
dylanhouxinglin <houxinglin@gmai.com>
2021-10-20 12:10:11 +0800

    更新35-二分查找

--- Go/ArrayList/35-搜索插入位置.go
+++ Go/ArrayList/35-搜索插入位置.go
@@ -16,3 +16,19 @@ func searchInsert(nums []int, target int) int {
 	}
 	return len(nums)
 }
+
+// 二分法, 二分前提：有序&&无重复
+func searchInsert2(nums []int, target int) int {
+	left, right := 0, len(nums)-1
+	for left <= right {
+		mid := (left+right)/2
+		if target < nums[mid] {
+			right = mid-1
+		} else if target > nums[mid] {
+			left = mid+1
+		} else {
+			return nums[mid]
+		}
+	}
+	return len(nums)
+}
